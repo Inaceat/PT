4f01fc2b097a160e0cd3590abd6c776d23b331ac
inaceat <inaceat@gmail.com>
2020-06-01 22:04:09 +0300

    [3]Lab 3 added.

---
+++ Lab3/MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Lab3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.transformButton = new System.Windows.Forms.Button();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textInput
+            // 
+            this.textInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textInput.Location = new System.Drawing.Point(0, 0);
+            this.textInput.Multiline = true;
+            this.textInput.Name = "textInput";
+            this.textInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textInput.Size = new System.Drawing.Size(371, 304);
+            this.textInput.TabIndex = 0;
+            this.textInput.TextChanged += new System.EventHandler(this.TextInput_TextChanged);
+            // 
+            // textOutput
+            // 
+            this.textOutput.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textOutput.BackColor = System.Drawing.SystemColors.Window;
+            this.textOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textOutput.Location = new System.Drawing.Point(377, 0);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.ReadOnly = true;
+            this.textOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textOutput.Size = new System.Drawing.Size(375, 304);
+            this.textOutput.TabIndex = 1;
+            // 
+            // transformButton
+            // 
+            this.transformButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.transformButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.transformButton.Location = new System.Drawing.Point(286, 367);
+            this.transformButton.Name = "transformButton";
+            this.transformButton.Size = new System.Drawing.Size(180, 46);
+            this.transformButton.TabIndex = 2;
+            this.transformButton.Text = "Transform";
+            this.transformButton.UseVisualStyleBackColor = true;
+            this.transformButton.Click += new System.EventHandler(this.TransformButton_Click);
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.BlinkRate = 0;
+            this.errorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProvider.ContainerControl = this;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 473);
+            this.Controls.Add(this.transformButton);
+            this.Controls.Add(this.textOutput);
+            this.Controls.Add(this.textInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "Lab3";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.TextBox textOutput;
+        private System.Windows.Forms.Button transformButton;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
+
---
+++ Lab3/MainForm.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _errorList = new List<string>();
+
+            _validationChecks = new Dictionary<Func<string, bool>, string>
+            {
+                {NotEmpty, "Empty input"},
+                {LessThan200Symbols, "Input exceeds 200 symbols"},
+                {NoNewLines, "New lines in input"},
+                {FirstSymbolIsCapitalLetter, "First symbol is not capital letter"},
+                {PunctuationImmediatelyAfterWord, "Punctuation mark is not after word"},
+                {NoAdjacentSpaces, "Two spaces in a row"}
+            };
+
+
+
+
+            ValidateInputText();
+        }
+
+
+
+        private void TransformButton_Click(object sender, System.EventArgs e)
+        {
+            TransformInput();
+        }
+
+        private void TextInput_TextChanged(object sender, System.EventArgs e)
+        {
+            ValidateInputText();
+        }
+
+
+
+        private readonly List<string> _errorList;
+        private readonly Dictionary<Func<string, bool>, string> _validationChecks;
+
+        private void ValidateInputText()
+        {
+            GetInputErrors();
+
+
+            if (0 == _errorList.Count)
+            {
+                errorProvider.Clear();
+                transformButton.Enabled = true;
+            }
+            else
+            {
+                var errors = new StringBuilder();
+
+
+                foreach (var errorText in _errorList) 
+                    errors.AppendLine(errorText);
+
+
+                errorProvider.SetError(transformButton, errors.ToString());
+                transformButton.Enabled = false;
+            }
+        }
+
+        private void GetInputErrors()
+        {
+            _errorList.Clear();
+
+            foreach (var check in _validationChecks)
+            {
+                if (!check.Key(textInput.Text))
+                    _errorList.Add(check.Value);
+            }
+        }
+
+
+
+        private bool NotEmpty(string text)
+        {
+            return 0 != text.Length;
+        }
+
+        private bool LessThan200Symbols(string text)
+        {
+            return 200 >= text.Length;
+        }
+        
+        private bool NoNewLines(string text)
+        {
+            return !text.Contains(Environment.NewLine);
+        }
+        
+        private bool FirstSymbolIsCapitalLetter(string text)
+        {
+            return text.Length == 0 || char.IsUpper(text[0]);
+        }
+
+        private bool PunctuationImmediatelyAfterWord(string text)
+        {
+            if (0 == text.Length)
+                return true;
+            
+    
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (char.IsPunctuation(text[i]) && !char.IsLetterOrDigit(text[i - 1]))
+                    return false;
+            }
+        
+            return true;
+        }
+
+        private bool NoAdjacentSpaces(string text)
+        {
+            return !text.Contains("  ");
+        }
+
+
+        private void TransformInput()
+        {
+            var result = textInput.Text;
+
+            //If length is odd number, reomve substring starting with 2nd space
+            if (0 != textInput.Text.Length % 2)
+            {
+                int firstSpaceIndex = textInput.Text.IndexOf(" ", StringComparison.Ordinal);
+
+                if (firstSpaceIndex < textInput.Text.Length)
+                {
+                    int secondSpaceIndex = textInput.Text.IndexOf(" ", firstSpaceIndex + 1, StringComparison.Ordinal);
+
+                    if (-1 != secondSpaceIndex)
+                    {
+                        result = textInput.Text.Substring(0, secondSpaceIndex);
+                    }
+                }
+            }
+
+            //Each word on new line
+            textOutput.Text = Regex.Replace(result, @"( )(\w)", "$1" + Environment.NewLine + "$2");;
+        }
+    }
+}
